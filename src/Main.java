54887cc0a0b1396a8e8e26510546a81aef8a8476
Rivonaldo <rivo5353filho@gmail.com>
2025-07-18 20:33:46 -0300

    criação mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Aprenda os fundamentos de Java");
+        curso1.setCargaHoraria(8);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de JavaScript");
+        curso2.setDescricao("Aprenda os fundamentos de JavaScript");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Sessão de mentoria para tirar dúvidas sobre o Curso de Java");
+        mentoria1.setData(java.time.LocalDate.now());
+        Mentoria mentoria2 = new Mentoria();
+
+        mentoria2.setTitulo("Mentoria de Java");
+        mentoria2.setDescricao("Sessão de mentoria para tirar dúvidas sobre o Curso de Java");
+        mentoria2.setData(java.time.LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println("====================================================");
+        System.out.println(mentoria1);
+        System.out.println("====================================================");
+        System.out.println(curso2);
+        System.out.println("====================================================");
+        System.out.println(mentoria2);
+        
     }
+
 }
