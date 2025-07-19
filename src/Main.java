51d24298ab342f28b082b4596144f5b6c937c60b
Rivonaldo <rivo5353filho@gmail.com>
2025-07-19 10:26:05 -0300

    Finalização

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
@@ -15,24 +14,49 @@ public class Main {
         curso2.setDescricao("Aprenda os fundamentos de JavaScript");
         curso2.setCargaHoraria(6);
 
+        Conteudo conteudo = new Curso();
+
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("Mentoria de Java");
         mentoria1.setDescricao("Sessão de mentoria para tirar dúvidas sobre o Curso de Java");
         mentoria1.setData(java.time.LocalDate.now());
+        
         Mentoria mentoria2 = new Mentoria();
-
         mentoria2.setTitulo("Mentoria de Java");
         mentoria2.setDescricao("Sessão de mentoria para tirar dúvidas sobre o Curso de Java");
         mentoria2.setData(java.time.LocalDate.now());
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Venha aprender a desenvolver Java nesse bootcamp personalizado para impulsionar sua aprendizagem");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+        bootcamp.getConteudos().add(mentoria2);
 
-        System.out.println(curso1);
-        System.out.println("====================================================");
-        System.out.println(mentoria1);
-        System.out.println("====================================================");
-        System.out.println(curso2);
-        System.out.println("====================================================");
-        System.out.println(mentoria2);
+        Dev devRivonaldo = new Dev();
+        devRivonaldo.setNome("Rivonaldo");
+        devRivonaldo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos de Rivonaldo: \n" + devRivonaldo.getConteudosInscritos());
+        System.out.println("--------------------------------------------------------------------");
+        devRivonaldo.progredir();
+        System.out.println("Conteudos inscritos de Rivonaldo: \n" + devRivonaldo.getConteudosInscritos());
+        devRivonaldo.progredir();
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("Conteudos Concluidos de Rivonaldo: \n" + devRivonaldo.getConteudosConcluidos());
+        System.out.println("XP: " + devRivonaldo.calcularXp());
+        
+        System.out.println("--------------------------------------------------------------------");
+        Dev devLucas = new Dev();
+        devLucas.setNome("Lucas");
+        devLucas.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos de Lucas: \n" + devLucas.getConteudosInscritos());
+        devLucas.progredir();
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("Conteudos inscritos de Lucas: \n" + devLucas.getConteudosInscritos());
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("Conteudos Concluidos de Lucas: \n" + devLucas.getConteudosConcluidos());
+        System.out.println("XP: " + devLucas.calcularXp());
         
     }
 
